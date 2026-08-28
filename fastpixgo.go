078f1e9a417b201d9cfa9d1f4f9d09e6884a7c4b
Tharun Budidha <tharun.b@fastpix.io>
2026-08-28 14:13:11 +0530

    Bump SDK version to 1.1.6

--- fastpixgo.go
+++ fastpixgo.go
@@ -193,9 +193,9 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *Fastpixgo {
 	sdk := &Fastpixgo{
-		SDKVersion: "1.1.5",
+		SDKVersion: "1.1.6",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:  "fastpix-sdk/go 1.1.5 2.797.1 1.0.0 github.com/FastPix/fastpix-go",
+			UserAgent:  "fastpix-sdk/go 1.1.6 2.797.1 1.0.0 github.com/FastPix/fastpix-go",
 			ServerList: ServerList,
 		},
 		hooks: hooks.New(),
